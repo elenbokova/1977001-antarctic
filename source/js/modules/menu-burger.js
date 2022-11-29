a2b56febbefe25eb474f14028453af8a5987ace9
elenbokova <nansydrew@mail.ru>
2022-11-30 01:59:20 +0400

    Add contacts and make edits before checking

--- source/js/modules/menu-burger.js
+++ source/js/modules/menu-burger.js
@@ -1,3 +1,9 @@
+import {FocusLock} from '../utils/focus-lock.js';
+import {ScrollLock} from '../utils/scroll-lock.js';
+
+const focusLock = new FocusLock();
+const scrollLock = new ScrollLock();
+
 const header = document.querySelector('[data-id="header"]');
 const navigationToggle = header.querySelector('[data-nav-toggle]');
 const navigation = header.querySelector('[data-nav]');
@@ -9,8 +15,12 @@ navigation.dataset.nav = 'is-hidden';
 const onBurgerClick = () => {
   if (navigation.dataset.nav === 'is-hidden') {
     navigation.dataset.nav = 'is-shown';
+    focusLock.lock('.header');
+    scrollLock.disableScrolling();
   } else {
     navigation.dataset.nav = 'is-hidden';
+    focusLock.unlock();
+    scrollLock.enableScrolling();
   }
 };
 
