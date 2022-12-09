4befb180c8da302fc607205105b22174e33b5a9f
elenbokova <nansydrew@mail.ru>
2022-12-09 17:59:32 +0400

    Fix bugs after testing

--- source/js/modules/menu-burger.js
+++ source/js/modules/menu-burger.js
@@ -7,20 +7,47 @@ const scrollLock = new ScrollLock();
 const header = document.querySelector('[data-id="header"]');
 const navigationToggle = header.querySelector('[data-nav-toggle]');
 const navigation = header.querySelector('[data-nav]');
+const navitaionLinks = header.querySelectorAll('[data-nav-link]');
 
 header.dataset.js = 'true';
 navigation.dataset.nav = 'is-hidden';
 
+const openMenu = () => {
+  navigation.dataset.nav = 'is-shown';
+  focusLock.lock('.header');
+  scrollLock.disableScrolling();
+  header.dataset.overlay = 'is-shown';
+  document.addEventListener('click', onClickOutsideMenu);
+};
+
+const closeMenu = () => {
+  navigation.dataset.nav = 'is-hidden';
+  focusLock.unlock();
+  scrollLock.enableScrolling();
+  header.dataset.overlay = 'is-hidden';
+  document.removeEventListener('click', onClickOutsideMenu);
+};
+
+const onClickOutsideMenu = (evt) => {
+  const target = evt.target;
+  if (!target.closest('.header__container')) {
+    closeMenu();
+  }
+};
+
+navitaionLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (navigation.dataset.nav === 'is-shown') {
+      closeMenu();
+    }
+  });
+});
 
 const onBurgerClick = () => {
   if (navigation.dataset.nav === 'is-hidden') {
-    navigation.dataset.nav = 'is-shown';
-    focusLock.lock('.header');
-    scrollLock.disableScrolling();
+    openMenu();
   } else {
-    navigation.dataset.nav = 'is-hidden';
-    focusLock.unlock();
-    scrollLock.enableScrolling();
+    closeMenu();
   }
 };
 
